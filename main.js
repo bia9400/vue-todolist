e837cccedba7365571f4d00ca8e96def7acdf68e
bia9400 <ferro.biagio@gmail.com>
2022-06-13 15:32:53 +0200

    update

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
 new Vue({
-    el:"#app"
+    el: "#app",
+    data: {
+        selected: "",
+        products: [
+            { id: 1, name: 'Viaggio' },
+            { id: 2, name: 'Spesa' },
+            { id: 3, name: 'Portfolio' },
+
+        ],
+        plusButtonClicked: false,
+        arrayData: [
+            {
+                taskType: "Viaggio",
+                taskChecked: false,
+                taskText: "Comprare scarpe a Bali",
+            },
+            {
+                taskType: "Viaggio",
+                taskChecked: false,
+                taskText: "Fare Paracadute",
+            },
+            {
+                taskType: "Spesa",
+                taskChecked: false,
+                taskText: "Comprare le gocciole",
+            }
+        ],
+    },
+    methods: {
+        plusClick() {
+            this.plusButtonClicked = true
+        },
+       /*  filterTrip() {
+            let x = this.arrayData.filter(student => student.taskType == "Viaggio")
+            
+            return x
+        },
+        filterSpesa(){
+            let x = this.arrayData.filter(student => student.taskType == "Spesa")
+            
+            return x
+        },
+        filterPortfolio(){
+            let x = this.arrayData.filter(student => student.taskType == "Portfolio")
+            
+            return x
+        }, */
+        checkboxControl(index){
+            if(this.arrayData[index].taskChecked==false)
+            {this.arrayData[index].taskChecked=true}
+            else{
+                this.arrayData[index].taskChecked=false
+            }
+        }
+    },
+
+
 })
